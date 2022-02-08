a1dc6cd0900bffc97a82323ecc03d7780bbc687a
david-lafontant <lafontant.david@gmail.com>
2022-02-08 13:34:42 -0600

    Add displayLeader module

---
+++ src/modules/displayLeader.js
@@ -0,0 +1,25 @@
+const displayLeader = () => {
+  const newLeader = document.querySelector('.table_item');
+
+  while (newLeader.hasChildNodes()) {
+    newLeader.removeChild(newLeader.lastChild);
+  }
+
+
+  const leaders = JSON.parse(localStorage.getItem('leaders'));
+
+
+  leaders.forEach((book, index) => {
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+              <td>${book.name}</td>
+              <td>: ${book.score}</td>
+          `;
+    newLeader.appendChild(newRow);
+
+
+  })
+
+}
+
+export default displayLeader;
